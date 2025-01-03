cc680b86211b5e578e567a1a622a11695067f4e7
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2025-01-02 21:01:20 -0500

    update

--- Project_Transport_Car/main/gameObjects/playerEnemies/player_enemy_shoot.go
+++ Project_Transport_Car/main/gameObjects/playerEnemies/player_enemy_shoot.go
@@ -1,6 +1,45 @@
 components {
   id: "enemy_shoot"
   component: "/main/_scripts/enemy_shoot.script"
+  properties {
+    id: "speed"
+    value: "1000.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "life"
+    value: "1.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "bulletDamage"
+    value: "50.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "bulletMask"
+    value: "player"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "raytraceMask"
+    value: "car"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "target"
+    value: "/player/player"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "radius"
+    value: "500.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+}
+components {
+  id: "enemy_bullet"
+  component: "/main/factories/enemy_bullet.factory"
 }
 embedded_components {
   id: "sprite"
@@ -19,12 +58,12 @@ embedded_components {
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
-  "mass: 1.0\n"
+  data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"default\"\n"
-  "mask: \"default\"\n"
+  "group: \"enemy\"\n"
+  "mask: \"playerBullet\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
